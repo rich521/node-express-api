91b478b078acc7a7ac590355087991ec75bddaa8
Richard Strutt <richardastrutt@gmail.com>
2017-07-17 23:08:33 +0100

    Added modelclass, and email key@

--- server/models/user.js
+++ server/models/user.js
@@ -3,10 +3,12 @@ const Schema = mongoose.Schema;
 
 // Define
 const userSchema = new Schema({
-  user: { type: String, unique: true, lowercase: true },
+  email: { type: String, unique: true, lowercase: true },
   password: String,
 });
 
 // Model Class
+const ModelClass = mongoose.model('user', userSchema);
 
 // Export model
+module.exports = ModelClass; 
