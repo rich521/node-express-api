b7073b97106a2e3d47c485f4784490bceecac84c
Richard Strutt <richardastrutt@gmail.com>
2017-07-19 23:21:53 +0100

    Added bcypt compare function

--- server/models/user.js
+++ server/models/user.js
@@ -24,6 +24,16 @@ userSchema.pre('save', function (next) {
   });
 });
 
+userSchema.method.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
+
 // Model Class
 const UserClass = mongoose.model('user', userSchema);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,8 +1,23 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
 const User = require('../models/user');
 const config = require('../config');
+
+// Create LocalStrategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+  // verification
+  User.findOne({ email }, (err, user) => {
+    if (err) return done();
+
+    if (!user) return done(null, false);
+
+    // compare passwords hash vs input password
+  });
+});
 
 // Setup options for JWT Strategty
 const jwtOptions = {
